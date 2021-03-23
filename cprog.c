ba0b518754dff96fc6c1d2fe8fb33584f83b1dc2
waterdark-droid <81210443+waterdark-droid@users.noreply.github.com>
2021-03-23 11:50:40 +0530

    Addition.c

---
+++ cprog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter number 1 : ");
+    scanf("%d",&a);
+    printf("Enter number 2 : ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("Addition of two numbers : %d",c);
+  return 0;
+}
